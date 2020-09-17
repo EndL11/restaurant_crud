18ff6565e611a49203132aeeeb42d30589a38a74
EndL11 <podobailo.andriy@gmail.com>
2020-09-17 12:33:04 +0300

    added comments to deleting func

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -17,7 +17,11 @@ import {
 } from "../../store/actions/state";
 import { MenuItem } from "../MenuItem";
 import { ModalWindow } from "../ModalWindow";
-import { updateOrder, loadOrders, deleteOrder } from "../../store/actions/order";
+import {
+  updateOrder,
+  loadOrders,
+  deleteOrder,
+} from "../../store/actions/order";
 
 const Menu = (props) => {
   const dispatch = useDispatch();
@@ -50,6 +54,7 @@ const Menu = (props) => {
   const onDeleteDish = (id) => {
     const newList = orderList;
     const deletingDish = menu.find((item) => item.id === id);
+    //  finding deleting dish in orders
     for (let i = 0; i < newList.length; i++) {
       let hasDeletedItem = false;
       let countOfDeletedDish = 0;
@@ -58,20 +63,24 @@ const Menu = (props) => {
         if (deletingDish.name === newList[i].orderArray[j].name) {
           hasDeletedItem = true;
           countOfDeletedDish += newList[i].orderArray[j].count;
+          //  filter order's dishes list
           newList[i].orderArray = newList[i].orderArray.filter(
             (item) => item.name !== deletingDish.name
           );
-          if(newList[i].orderArray.length === 0){
+          //  if after filtering dishes list empty - delete order
+          if (newList[i].orderArray.length === 0) {
             dispatch(deleteOrder(newList[i].id));
             orderDeleting = true;
           }
         }
       }
-      if (hasDeletedItem && orderDeleting == false) {
-        newList[i].totalPrice -= (countOfDeletedDish * deletingDish.price);
+      //  update order if it isn't deleted
+      if (hasDeletedItem && orderDeleting === false) {
+        newList[i].totalPrice -= countOfDeletedDish * deletingDish.price;
         dispatch(updateOrder(newList[i]));
       }
     }
+    //  delete dish from store
     dispatch(deleteDish(id));
   };
 
@@ -87,62 +96,54 @@ const Menu = (props) => {
     handleModal();
   };
 
-  const list = menu.map((item) => (
-    <MenuItem
-      key={item.menuId}
-      item={item}
-      onDeleteDish={onDeleteDish}
-      onEditDish={onEditDish}
-    />
-  ));
-
+  
   const onAddDish = (e) => {
     e.preventDefault();
     let name = e.target.name.value;
     let price = Number(e.target.price.value);
-
+    
     if (
       name.split(" ").join("") === "" ||
       price <= 0 ||
       isNaN(price) === true
-    ) {
-      alert("Wrong data!");
-      return;
-    }
-
-    // compare the same dish
-    let theSameDish = null;
-    if (menu !== null) {
-      theSameDish = menu.filter((element) => element.name === name).length;
-    }
-
-    if (theSameDish > 0) {
-      alert("Wrong data! Dish is exists!");
-      return;
-    }
-
-    let newDish = { menuId: props.idGenerator(), name, price };
-
-    dispatch(addDish(newDish));
-    handleModal();
-    dispatch(loadMenu());
-  };
-
-  const onSubmitEditing = (e) => {
-    e.preventDefault();
-    dispatch(loadMenu());
-    const editingDishIndex = menu.findIndex(
-      (item) => item.id === editingDish.id
-    );
-    for (let i = 0; i < menu.length; i++) {
-      if (editingDish.name === menu[editingDishIndex].name) continue;
-
+      ) {
+        alert("Wrong data!");
+        return;
+      }
+      
+      // compare the same dish
+      let theSameDish = null;
+      if (menu !== null) {
+        theSameDish = menu.filter((element) => element.name === name).length;
+      }
+      
+      if (theSameDish > 0) {
+        alert("Wrong data! Dish is exists!");
+        return;
+      }
+      
+      let newDish = { menuId: props.idGenerator(), name, price };
+      
+      dispatch(addDish(newDish));
+      handleModal();
+      dispatch(loadMenu());
+    };
+    
+    const onSubmitEditing = (e) => {
+      e.preventDefault();
+      dispatch(loadMenu());
+      const editingDishIndex = menu.findIndex(
+        (item) => item.id === editingDish.id
+        );
+        for (let i = 0; i < menu.length; i++) {
+          if (editingDish.name === menu[editingDishIndex].name) continue;
+          
       if (menu[i].name === editingDish.name) {
         alert("Dish with this name already exists!");
         return;
       }
     }
-
+    
     // updating orderlist
     let newList = orderList;
     for (let i = 0; i < newList.length; i++) {
@@ -167,22 +168,31 @@ const Menu = (props) => {
             return prev + curr.count * menuItem.price;
           },
           0
-        );
+          );
+        }
+        if (priceChanged || nameChanged) {
+          dispatch(updateOrder(newList[i]));
+        }
       }
-      if (priceChanged || nameChanged) {
-        dispatch(updateOrder(newList[i]));
-      }
-    }
-    dispatch(updateMenu(editingDish));
-    dispatch(resetEditingObject());
-    handleModal();
-  };
-
-  let modalContent, modalHideAction, modalTitle;
-
-  if (isEditing) {
-    modalContent = (
-      <Form onSubmit={onSubmitEditing}>
+      dispatch(updateMenu(editingDish));
+      dispatch(resetEditingObject());
+      handleModal();
+    };
+    
+    const list = menu.map((item) => (
+      <MenuItem
+        key={item.menuId}
+        item={item}
+        onDeleteDish={onDeleteDish}
+        onEditDish={onEditDish}
+      />
+    ));
+    
+    let modalContent, modalHideAction, modalTitle;
+    
+    if (isEditing) {
+      modalContent = (
+        <Form onSubmit={onSubmitEditing}>
         <Form.Group controlId="formBasicText">
           <Form.Label>Name</Form.Label>
           <Form.Control
